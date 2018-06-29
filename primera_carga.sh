3cdcc018bbfeaab978979643add2d3d246f7b123
Emilio Gª Loeda <emilio.loeda@gmail.com>
2018-06-29 21:52:05 +0200

    Create primera_carga.sh

---
+++ primera_carga.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/egloeda/ubuntu/master/primera_carga.sh)"
+# sh -c "$(wget https://raw.githubusercontent.com/egloeda/ubuntu/master/primera_carga.sh -O -)"
+
+apt-get -y update
+apt-get -y upgrade
+
+cd /root
+wget http://www.webmin.com/jcameron-key.asc
+apt-key add jcameron-key.asc 
+
+touch /etc/apt/sources.list.d/webmin.list
+echo "deb http://download.webmin.com/download/repository sarge contrib" >/etc/apt/sources.list.d/webmin.list
+
+apt-get -y install apt-transport-https
+apt-get -y update
+apt-get -y install webmin
