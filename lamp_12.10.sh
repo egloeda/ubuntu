0bfa5aecc2f5bfa1e5ee411612982aa6106a2e2f
Emilio Gª Loeda <emilio.loeda@gmail.com>
2018-06-29 22:48:47 +0200

    Update lamp_12.10.sh

--- lamp_12.10.sh
+++ lamp_12.10.sh
@@ -8,7 +8,7 @@ apt-get -y install php5 php5-dev libapache2-mod-php5
 apt-get -y install php5-gd php5-gmp php5-mysql php5-recode php5-snmp  php5-snmp php5-ming php5-mysqlnd php5-adodb  php5-intl php5-curl php5-mcrypt php5-imagick
 apt-get -y install postfix
 apt-get -y install snmp snmp-mibs-downloader snmpd
-echo "<?php phpinfo(); ?>" > /var/www/html/p1.php
+echo "<?php phpinfo(); ?>" > /var/www/p1.php
 
 service apache2 restart
 service mysql restart
