525b3d8653e8d4ff7c9e9b40f87db3e3b989d76d
Emilio Gª Loeda <emilio.loeda@gmail.com>
2018-06-29 22:43:02 +0200

    Update lamp_12.10.sh

--- lamp_12.10.sh
+++ lamp_12.10.sh
@@ -1,19 +1,14 @@
 #!/bin/sh
 
-# sh -c "$(wget https://raw.githubusercontent.com/egloeda/debian/master/debian9_lamp.sh -O -)"
+# sh -c "$(wget https://raw.githubusercontent.com/egloeda/ubuntu/master/lamp_12.10.sh -O -)"
 
-apt-get -y install apache2-ssl-dev libapache2-mod-php php mariadb-server
-apt-get -y install php-gd php-readline php-mbstring php-curl php-mysql
+apt-get -y install mysql-server-5.5
+apt-get -y install apache2 
+apt-get -y install php5 php5-dev libapache2-mod-php5
+apt-get -y install php5-gd php5-gmp php5-mysql php5-recode php5-snmp  php5-snmp php5-ming php5-mysqlnd php5-adodb  php5-intl php5-curl php5-mcrypt php5-imagick
 apt-get -y install postfix
 apt-get -y install snmp snmp-mibs-downloader snmpd
 echo "<?php phpinfo(); ?>" > /var/www/html/p1.php
-
-mkdir -p /software/{mantisbt,dokuwiki}
-mkdir -p /var/www/html/{prod,test}/{mantis,faq}
-cd /sofrware/mantisbt
-wget https://sourceforge.net/projects/mantisbt/files/mantis-stable/2.8.0/mantisbt-2.8.0.tar.gz/download
-mv download mantisbt-2.8.0.tar.gz
-
 
 service apache2 restart
 service mysql restart
