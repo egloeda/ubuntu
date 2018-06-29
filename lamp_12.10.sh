52f77180da6cf9de7e67a1927126c229bbe39f18
Emilio Gª Loeda <emilio.loeda@gmail.com>
2018-06-29 23:10:20 +0200

    Update lamp_12.10.sh

--- lamp_12.10.sh
+++ lamp_12.10.sh
@@ -5,7 +5,9 @@
 apt-get -y install mysql-server-5.5
 apt-get -y install apache2 
 apt-get -y install php5 php5-dev libapache2-mod-php5
-apt-get -y install php5-gd php5-gmp php5-recode php5-snmp  php5-snmp php5-ming php5-mysqlnd php5-adodb  php5-intl php5-curl php5-mcrypt php5-imagick
+apt-get -y install php5-gd php5-gmp php5-recode php5-snphp-dbmp  php5-snmp php5-ming php5-mysqlnd php5-adodb  php5-intl php5-curl php5-mcrypt php5-imagick
+apt-get -y install php-db php5-sybasefreetds-dev
+apt-get -y install freetds-dev sqsh
 apt-get -y install postfix
 apt-get -y install snmp snmp-mibs-downloader snmpd
 echo "<?php phpinfo(); ?>" > /var/www/p1.php
